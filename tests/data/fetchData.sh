444254161a36c4192e07dc36cce7d97efe9c5781
Arnav <ocean57@gmail.com>
2020-08-04 02:57:09 -0500

    Added yeast files

--- tests/data/fetchData.sh
+++
@@ -1,6 +0,0 @@
-wget HCT116-HyPBase_sort_canon.ccf.gz
-wget HCT116-PBase_unsorted.ccf.gz
-wget HCT116-SP1-PBase_sort_canon.ccf.gz
-wget HCT116-PBase_sort_canon.ccf.gz
-wget HCT116-SP1-HyPBase_sort_canon.ccf.gz
-wget uniform_test.ccf.gz
