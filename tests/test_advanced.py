9007b81401e69b11babcf579849bd4c147e051e5
Arnav <ocean57@gmail.com>
2020-08-05 00:54:17 -0500

    Updated test suites for CI

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -5,14 +5,13 @@ import tests.test_utilities as test_utilities
 import sys
 import unittest
 
-
 # Enable warnings
 if not sys.warnoptions:
     import os
     import warnings
-    warnings.simplefilter("default") # Change the filter in this process
-    os.environ["PYTHONWARNINGS"] = "default" # Also affect subprocesses
 
+    warnings.simplefilter("default")  # Change the filter in this process
+    os.environ["PYTHONWARNINGS"] = "default"  # Also affect subprocesses
 
 # Create an advanced test suite
 advanced_tests = unittest.TestSuite()
@@ -57,3 +56,9 @@ advanced_tests.addTests(
 # Run tests
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(advanced_tests)
+# Running a suite will not properly propagate error code
+# See https://code-maven.com/python-unittest-fails-exit-code-0
+if result.wasSuccessful():
+    exit(0)
+else:
+    exit(1)
